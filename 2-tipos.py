0e35320360a5e33274bc6daf8b24162371177e19
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-25 20:01:01 -0300

    learning types

---
+++ 2-tipos.py
@@ -0,0 +1,15 @@
+name = "Resident Evil 4 Remake"
+launch_year = 2023
+game_price = 300.00
+plan_included = False
+
+print(name)
+print(launch_year)
+print(game_price)
+print(plan_included)
+
+print(type(name))
+print(type(launch_year))
+print(type(game_price))
+print(type(plan_included))
+
