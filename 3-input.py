1eddae586be45e2c69a04b6522c9748631b8a0b9
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-26 18:59:12 -0300

    input

---
+++ 3-input.py
@@ -0,0 +1,16 @@
+# Utilizando o Input
+'''
+Jogoteca
+'''
+
+name = input("Digite o nome do jogo\n")
+print(name)
+
+launch_year = int(input("Digite o ano de lançamento do jogo:\n"))
+print(launch_year)
+
+game_price = float(input("Digite o preço do jogo:\n"))
+print(game_price)
+
+plan_included = (input("Está incluso no serviço mensal?\n"))
+print(plan_included)
