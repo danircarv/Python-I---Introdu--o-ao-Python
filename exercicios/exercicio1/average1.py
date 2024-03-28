797b92bf4b6b042704050e7fae12523fa766a2a1
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-28 09:45:36 -0300

    correcting the media calc

--- exercicios/exercicio1/average1.py
+++ exercicios/exercicio1/average1.py
@@ -3,6 +3,6 @@ nota2 = float(input("Digite a sua segunda nota:\n"))
 nota3 = float(input("Digite a sua terceira nota:\n"))
 nota4 = float(input("Digite a sua quarta nota:\n"))
 
-media = (nota1 + nota2 + nota3 + nota4)/41
+media = (nota1 + nota2 + nota3 + nota4)/4
 
 print(f"A média das notas é {media:.2f}")
