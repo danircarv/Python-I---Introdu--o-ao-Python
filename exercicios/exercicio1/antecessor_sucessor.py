0f0582e0ce3af803c1b2fe2a86afa51982da13de
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-28 09:45:08 -0300

    exercise 1 - average and predecessor, sucessor

---
+++ exercicios/exercicio1/antecessor_sucessor.py
@@ -0,0 +1,3 @@
+num1 = int(input("Digite o número:\n"))
+print(f"O antecessor de {num1} é {(num1 - 1)}")
+print(f"O sucessor de {num1} é {(num1 + 1)}")
---
+++ exercicios/exercicio1/average1.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a sua primeira nota:\n"))
+nota2 = float(input("Digite a sua segunda nota:\n"))
+nota3 = float(input("Digite a sua terceira nota:\n"))
+nota4 = float(input("Digite a sua quarta nota:\n"))
+
+media = (nota1 + nota2 + nota3 + nota4)/41
+
+print(f"A média das notas é {media:.2f}")
