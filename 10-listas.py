1091169ca91ab27608c48cb20e1ef3555cc72ab9
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-29 07:59:29 -0300

    learning lists

---
+++ 10-listas.py
@@ -0,0 +1,20 @@
+game_fifa = ["Fifa 23", 2022, 90.50, True]
+print(game_fifa)
+
+games_list = ["Resident Evil", "Star Wars",
+              "Zelda", "Red Dead", "Mario"]
+
+print(games_list)
+
+# 1 - Buscar os dois primeiros items da lista
+print(games_list[0:2])
+
+# 2 - Buscar o ultimo item da lista
+print(games_list[-1])
+
+# 3 - Buscar jogos até uma determinada posição
+print(games_list[:3])
+
+# 4 - Buscar jogos de uma posição em diante
+print(games_list[1:4])
+
---
+++ metodos_list.py
@@ -0,0 +1,27 @@
+
+
+games_list = ["Resident Evil", "Star Wars",
+              "Zelda", "Red Dead", "Mario", "Luigi"]
+
+# 1 - Tamanho da Lista
+print(len(games_list))
+
+# 2 - Recuperar um item da lista pelo índice
+print(games_list.index("Mario"))
+
+# 3 - Adicionar item ao final da lista
+games_list.append("GTA")
+print(games_list)
+
+# 4 - Ordenar a lista
+games_list.sort()
+print(games_list)
+
+# 5 - Copiar items de uma lista para outra
+games_finished = games_list.copy()
+games_finished.remove("Star Wars")
+print(games_finished)
+
+# 6 - Remove todos os itens da lista
+games_list.clear()
+print(games_list)
