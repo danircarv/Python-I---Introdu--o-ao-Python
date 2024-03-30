2e8f433eabd7c7d031ae4f24947d3baab18ee6a1
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-30 17:09:07 -0300

    learning list methods and tuples

---
+++ 12-tuplas.py
@@ -0,0 +1,28 @@
+games_tuple = ("Fifa", "Red Dead", "Star Wars"
+               , "Mario", "Zelda")
+print(games_tuple)
+print(type(games_tuple))
+
+# 1 - Buscar os dois primeiros itens da tupla
+print(games_tuple[:2])
+
+# 2 - Buscar o último item da lista
+print(games_tuple[-1])
+
+# 3 Buscar jogos até uma determinada posição
+print(games_tuple[:3])
+
+# 4 - Buscar jogos de uma posição em diante
+print(games_tuple[2:])
+
+# 5 - Recuperar um item da tupla pelo indice
+print(games_tuple.index("Mario"))
+
+
+
+# - Não possibilita adicionar valores na tupla
+# - Não possibilita remover valores na tupla
+# - Não possibilita ordenar valores na tupla
+
+
+
