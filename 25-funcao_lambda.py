05af8efe5dac46f87bbbcd631a8fbeb3dbdc87ff
Daniel Carvalho <danielrodriguescarvalho777@gmail.com>
2024-04-01 18:28:42 -0300

    learning function lambda

---
+++ 25-funcao_lambda.py
@@ -0,0 +1,16 @@
+# 1 - Função de potência de número
+power = lambda num: num**2
+
+# 2 - Função que verifica se o número é par
+even = lambda x: x % 2 == 0
+
+# 3 - Função que divide o número por outro
+divNum = lambda x,y : x/y
+
+# 4 - Função que inverte uma string
+reverse = lambda s: s[::-1]
+
+print(power(3))
+print(even(3))
+print(divNum(33,4))
+print(reverse("Javascript"))
