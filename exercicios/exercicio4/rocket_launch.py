2d04454ad32e8f2f5fcb1bfd03b61a30b30d5dc6
Daniel Carvalho <danielrodriguescarvalho777@gmail.com>
2024-04-02 13:48:46 -0300

    correcting exercise

--- exercicios/exercicio4/rocket_launch.py
+++ exercicios/exercicio4/rocket_launch.py
@@ -3,7 +3,7 @@ import winsound
 for i in range(11):
     if i >= 0:
         print(10-i)
-        winsound.Beep(800, 500)
+        winsound.Beep(800, 1000)
 print("beep")
 winsound.Beep(900, 1500)
     
